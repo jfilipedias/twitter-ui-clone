609d0ee7ffa0d1629bcd22f8dc8f51d9ee85b73b
Filipe Dias <filipediascontato@gmail.com>
2022-04-07 13:53:14 -0300

    Creates the Layout component

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Feed from "../Feed";
+import { Container, Wrapper } from "./styles";
+
+const Layout: React.FC = () => {
+  return (
+    <Container>
+    </Container>
+  );
+};
+
+export default Layout;
